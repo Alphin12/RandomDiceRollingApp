43824064132f2d69aab4f665395302eac54163f1
Alphin K Sajan <76272357+Alphin12@users.noreply.github.com>
2023-05-08 20:35:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:diceapp/gradient_container.dart';   //coz we call tht custom widget here
+import 'package:diceapp/gradient_container.dart';  
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(                        // for good looking screen
+      home: Scaffold(                        
         body:
-            GradientContainer( [Color.fromARGB(255, 78, 18, 181),Color.fromARGB(255, 80, 0, 127)]), //calling the class we want to reuse ie calling custom widget, this is a constructor.
+            GradientContainer( [Color.fromARGB(255, 78, 18, 181),Color.fromARGB(255, 80, 0, 127)]), 
       ),
     ),
   );
