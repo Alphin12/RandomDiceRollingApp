b4dcfcf2fa2ab9c9022cb40bbcd4eef99ecaeeaf
Alphin K Sajan <76272357+Alphin12@users.noreply.github.com>
2023-05-08 20:36:39 +0530

    Update gradient_container.dart

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -7,23 +7,23 @@ const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget 
 {
-  const GradientContainer(this.colours,{super.key}); //is a ctor fn tht accepts a key and passes to its superclass StatelessWidget
+  const GradientContainer(this.colours,{super.key}); 
   
   final List<Color> colours;
  
 
   @override
-  Widget build(context) // build is called automatically by flutter.
+  Widget build(context) 
   {
-    return Container(                //after return paste the widgets we want to reuse everytime.
+    return Container(                
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: colours,
-          begin:startAlignment, // ie gradient starts from topleft to bottomright
+          begin:startAlignment, 
           end: endAlignment,
         ),
       ),
-      child:const Center(                                       //made it centre
+      child:const Center(                                   
       child: DiceRoller(),
       ),
     );
